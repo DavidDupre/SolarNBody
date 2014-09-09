03acf98d750f5a346ccc312171bf91b0a6c7d9b3
David Dupre <djdupre7@gmail.com>
2014-09-09 11:39:08 -0700

    Removed redundant objects

--- src/dump/Body.java
+++ src/dump/Body.java
@@ -8,26 +8,12 @@ import org.lwjgl.util.glu.Sphere;
 
 public class Body extends Thing{
 	// Variables from csv file
-	public int id = 0;
-	public String name;
 	public double mass = 1f;
 	public double radius = 1;
-	public double semiMajorAxis;
 	public double systemMass;
-	public double period;
 	public String type = "Moon";
 
-	public Vector3D velocity = new Vector3D();
-	public Vector3D position = new Vector3D();
-	public Vector3D acceleration = new Vector3D();
-	public Body parent;
-	public Sphere image = new Sphere();
 	public double sizeScale = 1;
-	public boolean hasTrail = false;
-	public Trail trail;
-	public float[] color = {1f,1f,1f};
-	public boolean drawBigSphere = false;
-	public boolean drawConic = true;
 	
 	public void setSize(double size) {
 		radius = size;
